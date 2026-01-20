66e9f98d014c7c2218158134f8537b85c331074b
toshisakao <toshi.sakao.r@gmail.com>
2026-01-20 15:29:15 +0100

    python things

--- init.lua
+++ init.lua
@@ -709,6 +709,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'pyright',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -871,7 +872,22 @@ require('lazy').setup({
   -- nvlime lisp
   {
     'monkoose/nvlime',
-    dependencies = { 'monkoose/parsley' }
+    dependencies = { 'monkoose/parsley' },
+  },
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
+    },
+    ft = 'python', -- Load when opening Python files
+    keys = {
+      { ',v', '<cmd>VenvSelect<cr>' }, -- Open picker on keymap
+    },
+    opts = { -- this can be an empty lua table - just showing below for clarity.
+      search = {}, -- if you add your own searches, they go here.
+      options = {}, -- if you add plugin options, they go here.
+    },
   },
 
   { -- You can easily change to a different colorscheme.
